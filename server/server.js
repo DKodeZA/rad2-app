4a72d18c5161e4b2fc634f216244e585b21f409d
Dylan <dylank@bbd.co.za>
2020-08-23 14:07:28 +0200

    changed scripts

--- server/server.js
+++ server/server.js
@@ -8,6 +8,14 @@ app.use(express.json());
 app.use(cors());
 app.use('/game-prize', gamePrizeAPI);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    })
+}
+
 app.listen(3000, () => {
     console.log('--Server has started--');
 });
