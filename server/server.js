b273567348f28f7d2bc28e6593badac1ee3ae524
Dylan <dylank@bbd.co.za>
2020-08-23 20:15:02 +0200

    changed scripts

--- server/server.js
+++ server/server.js
@@ -9,10 +9,10 @@ app.use(cors());
 app.use('/game-prize', gamePrizeAPI);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("frontend/build"));
+    app.use(express.static("dist/rad2-app"));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "dist", "rad2-app", "index.html"));
     })
 }
 
